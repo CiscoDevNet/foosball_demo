6470fcaba847a2812f0d82e0649c068fed63a0e1
JockDaRock <jock.da.rock@gmail.com>
2017-06-25 15:38:35 -0700

    Scoreboard update

--- api_service/rest_api_demo/api/restplus.py
+++ api_service/rest_api_demo/api/restplus.py
@@ -2,7 +2,7 @@ import logging
 import traceback
 
 from flask_restplus import Api
-from api_service.rest_api_demo import settings
+import settings
 from sqlalchemy.orm.exc import NoResultFound
 
 log = logging.getLogger(__name__)
